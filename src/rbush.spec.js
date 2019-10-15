b74d4a56a9a1541510d63607aeb9e3938ac27011
Maximiliano A. Fontana <fontana@csdisco.com>
2019-10-15 17:39:23 -0300

    Adding more tests

--- src/rbush.spec.js
+++ src/rbush.spec.js
@@ -53,6 +53,29 @@ describe('RBush', () => {
     expect(result.contains).to.have.length(0);
   });
 
+  it('should detect the selected cell doesnt intersect an existent redaction', () => {
+    tree.load([
+      // A1:D1
+      {
+        minX: 1,
+        minY: 1,
+        maxX: 4,
+        maxY: 1,
+      },
+    ]);
+
+    const result = tree.searchAll({
+      // E5
+      minX: 5,
+      minY: 5,
+      maxX: 5,
+      maxY: 5,
+    });
+
+    expect(result.intersects).to.have.length(0);
+    expect(result.contains).to.have.length(0);
+  });
+
   it('should detect the selected range is contained by an existent redaction', () => {
     tree.load([
       // A1:D4
@@ -93,6 +116,27 @@ describe('RBush', () => {
       minY: 1,
       maxX: 4,
       maxY: 4,
+    });
+
+  });
+
+  it('should detect a selected cell is contained in an existent redaction', () => {
+    tree.load([
+      // A1:D4
+      {
+        minX: 1,
+        minY: 1,
+        maxX: 4,
+        maxY: 4,
+      },
+    ]);
+
+    const result = tree.searchAll({
+      // A1
+      minX: 1,
+      minY: 1,
+      maxX: 1,
+      maxY: 1,
     });
 
     expect(result.intersects).to.have.length(0);
